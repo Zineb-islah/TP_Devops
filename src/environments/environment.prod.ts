cb7a7af1ac33be2cb7b7ff3f31d3c03c3d263873
Sara-Ing <saraaitbrik2002@gmail.com>
2024-12-30 17:50:01 +0100

    Ajout environment.pro

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,  // Active les optimisations pour la production
+    apiUrl: 'https://us-central1-junkielabs-57977.cloudfunctions.net/apiPortfolio', // URL de l'API
+    recaptcha: 'votre-cle-recaptcha-en-production' // Clé de reCAPTCHA en production
+  };
+  
